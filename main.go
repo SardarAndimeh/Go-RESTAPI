ae60dcd20579114d752f401160e7fa83d2860972
Sardar <sardar.andimeh7@gmail.com>
2024-06-16 15:38:35 +0200

    feat: init server requests

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	models "project/REST_API/events"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,11 +12,33 @@ func main() {
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
+	server.POST("/events", postEvents)
 
 	server.Run(":8080")
 
 }
 
 func getEvents(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{"message": "Hello"})
+
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events)
+}
+
+func postEvents(context *gin.Context) {
+
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": "could not parse the Post request"})
+		return
+	}
+
+	event.ID = 1
+	event.UserId = 1
+
+	event.Save()
+
+	context.JSON(http.StatusCreated, gin.H{"message ": "event created :)", "event": event})
+
 }
