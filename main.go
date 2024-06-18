5811b24934e8472511b1da33da9feb7eb2a2bf42
Sardar <sardar.andimeh7@gmail.com>
2024-06-18 14:37:44 +0200

    added untracked file into development branch

--- main.go
+++ main.go
@@ -2,20 +2,45 @@ package main
 
 import (
 	"net/http"
+	models "project/REST_API/events"
+
+	"project/REST_API/db"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 
+	db.InitDB()
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
+	server.POST("/events", postEvents)
 
 	server.Run(":8080")
 
 }
 
 func getEvents(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{"message": "Hello"})
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events)
+}
+
+func postEvents(context *gin.Context) {
+
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, err)
+		return
+	}
+
+	event.ID = 1
+	event.UserId = 1
+
+	event.Save()
+
+	context.JSON(http.StatusCreated, gin.H{"message ": "event created :)", "event": event})
+
 }
